cbdedf8d5e1466388d70f0d02b02aa46c1bf16ff
Gil Aguirre <gil.aguirre@gmail.com>
2022-05-04 15:21:01 -0300

    Primeiro Commit Webscrapper

--- src/WebScrapping/Main.php
+++ src/WebScrapping/Main.php
@@ -3,6 +3,9 @@
 namespace Galoa\ExerciciosPhp2022\WebScrapping;
 
 use DOMDocument;
+use DOMXpath;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Common\Entity\Row;
 
 /**
  * Runner for the Webscrapping exercice.
@@ -16,6 +19,27 @@ class Main {
     $dom = new DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../webscrapping/origin.html');
     (new Scrapper())->scrap($dom);
-  }
+    $domXPath = new DOMXPath($dom);
+    $content = $domXPath->query("//*[@class='paper-card p-lg bd-gradient-left']");
+    $writer = WriterEntityFactory::createXLSXWriter();
+    $writer->openToFile('./webscrap.xlsx');
+    
+    foreach ($content as $entry) {
+      $id = $entry->lastChild->lastChild->nodeValue;
+      $title = $entry->firstChild->nodeValue;
+      $type = $entry->lastChild->firstChild->nodeValue;
+      $authorsString = $entry->firstChild->nextSibling->nodeValue;
+      $authorsArray = explode("; ", $authorsString);
+      $institution = $entry->firstChild->nextSibling->firstChild->attributes[0]->nodeValue;
+      $paper = [$id , $title , $type];
+      foreach($authorsArray as $author){
+        $paper[] = $author;
+        $paper[] = $institution;
+      }
+      $rowFromValues = WriterEntityFactory::createRowFromArray($paper);
+      $writer->addRow($rowFromValues);
+    }
 
+    $writer->close();
+  }
 }
